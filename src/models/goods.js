d4965a6f17206af622fd007744e9ccafcac9a684
liukaiqin <1908936375@qq.com>
2019-07-27 09:52:52 +0800

    购物车总价

--- src/models/goods.js
+++ src/models/goods.js
@@ -1,6 +1,21 @@
 export default {
   namespace: 'goods',
   state: {
-    goods: [{}],
+    goods: [],
+  },
+
+  effects: {
+    //获取购物车商品信息
+    *getGoods(action, { put }) {
+      let response = yield fetch('http://localhost:3000/goods');
+      let res = yield response.json();
+
+      yield put({ type: 'setgoods', goods: res });
+    },
+    reducers: {
+      setgoods(state, action) {
+        return { ...state, ...{ goods: action.goods } };
+      },
+    },
   },
 };
